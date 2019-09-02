58811ca6121d2140f92e7091758cd96625d563f0
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-09-02 22:41:48 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -20,8 +20,9 @@ def reduce(array, starting_value=nil)
   
   while i < array.length
     #pass num1 and each ele to this block with yield keyword
-    #loop over array, pass in value of sum (sv or array[0]), add num to next ele  
+    #loop over array, pass in value of sum (sv or array[0]), add num to next ele in array and redefine sum 
     sum = yield(sum, array[i])
+    i += 1
   end
     
   
