44436c91a24fa5ffbbff2eb7f3ac5d7d1fe24d18
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-09-02 21:52:47 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,4 +1,4 @@
-def map(source_array,)
+def map(source_array,block)
   
 end 
 # ArgumentError: given 1= given an argument, but code you wrote isn't expecting an argument, fix by giving map an argument 
