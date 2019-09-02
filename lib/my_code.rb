4531af97bfbafe3d8f07ebae37f56653b7a1b98e
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-09-02 22:18:34 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,19 +1,13 @@
-def map(source_array) #method map accepts array
-  #map returns a new array of manipulated elem
-  #pass each ele in array to a block called on map method, by calling yield keywork and passing in each ele
+def map(source_array) 
   new_array = []
   i = 0
   while i < array.length 
     new_array.push(yield(array[i]))
     i += 1
   end
-  #add each element to new array and return new array at the end
-  # loop over array to manipulate every ele inside the array (as long as there are ele in array)
   new_array
-  #new_array = sourcearray.map {|num|}
 end 
 
-# ArgumentError: given 1= given an argument, but code you wrote isn't expecting an argument, fix by giving map an argument 
 
 # describe 'my own map' do
 #   it "returns an array with all values made negative" do
