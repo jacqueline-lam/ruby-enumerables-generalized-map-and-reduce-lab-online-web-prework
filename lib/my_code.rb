c9546843673fc6749e63081c68c6439e7a967f1f
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-09-02 22:09:18 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,7 +1,7 @@
 def map(source_array,block)
   #map returns a new array of manipulated elem
   new_array = []
-  while 
+  while i
   #new_array = sourcearray.map {|num|}
 end 
 # ArgumentError: given 1= given an argument, but code you wrote isn't expecting an argument, fix by giving map an argument 
