9c5d0f07dc168186be6dbfaab80c322bdd22d996
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-09-02 21:49:51 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,21 +3,21 @@ def my_own_map
 end 
 
 
-describe 'my own map' do
-  it "returns an array with all values made negative" do
-    expect(map([1, 2, 3, -9]){|n| n * -1}).to eq([-1, -2, -3, 9])
-  end
+# describe 'my own map' do
+#   it "returns an array with all values made negative" do
+#     expect(map([1, 2, 3, -9]){|n| n * -1}).to eq([-1, -2, -3, 9])
+#   end
 
-  it "returns an array with the original values" do
-    dune = ["paul", "gurney", "vladimir", "jessica", "chani"]
-    expect(map(dune){|n| n}).to eq(dune)
-  end
+#   it "returns an array with the original values" do
+#     dune = ["paul", "gurney", "vladimir", "jessica", "chani"]
+#     expect(map(dune){|n| n}).to eq(dune)
+#   end
 
-  it "returns an array with the original values multiplied by 2" do
-    expect(map([1, 2, 3, -9]){|n| n * 2}).to eq([2, 4, 6, -18])
-  end
+#   it "returns an array with the original values multiplied by 2" do
+#     expect(map([1, 2, 3, -9]){|n| n * 2}).to eq([2, 4, 6, -18])
+#   end
 
-  it "returns an array with the original values squared" do
-    expect(map([1, 2, 3, -9]){|n| n * n}).to eq([1, 4, 9, 81])
-  end
-end
+#   it "returns an array with the original values squared" do
+#     expect(map([1, 2, 3, -9]){|n| n * n}).to eq([1, 4, 9, 81])
+#   end
+# end
